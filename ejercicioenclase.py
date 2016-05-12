bf855b276fd9cd83d70cb5f5edd6ddcc68f8fb03
Alexander Figueroa <alexdavid2405@hotmail.es>
2016-05-12 08:19:16 -0500

    función for
    implementado para pedir un numero determinado de frases

--- ejercicioenclase.py
+++ ejercicioenclase.py
@@ -1,3 +1,5 @@
-cadena=input("Ingrese una frase: ")
-b=cadena.replace(" ","")
-print("Existen: "+str(len(b))+" letras.")
+a=int(input("Cuantas frases desea ingresar: "))
+for i in range(1,a+1):
+	cadena=input("Ingrese frase "+str(i)+" :")
+	b=cadena.replace(" ","")
+	print("Existen: "+str(len(b))+" letras.")
