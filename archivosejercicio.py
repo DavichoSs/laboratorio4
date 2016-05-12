3c409b0b131863166fe4d1a8dcfd3e4137698335
rubenpozo <ruben_oso28@hotmail.com>
2016-05-12 08:33:53 -0500

    menor código
    eliminada función de mas

--- archivosejercicio.py
+++ archivosejercicio.py
@@ -1,13 +1,7 @@
-def creartxt():
-    archivo=open('datos.txt','w')
-    archivo.close()
- 
 def grabartxt():
-    archivo=open('datos.txt','a')
+    archivo=open('datos.txt','w')
     archivo.write('Hola mundo\n')
     archivo.write('Mi nombre es Alexander\n')
     archivo.write('Mi compa se llama Ruben\n')
     archivo.close()
- 
-creartxt()
 grabartxt()
