3b92bbc4f1a6f118499fe8497eb6140e90f3aa7c
Alexander Figueroa <alexdavid2405@hotmail.es>
2016-05-12 08:59:50 -0500

    nuevo
    codificar

--- archivosejercicio.py
+++ archivosejercicio.py
@@ -1,10 +1,6 @@
 def grabartxt():
     archivo=open('datos.txt','w')
-<<<<<<< HEAD
-    archivo.write('Mi nombre es Alexander Mi compa se llama Ruben')
-=======
-    archivo.write('Hola mundo Mi nombre es Alexander Mi compa se llama Ruben Pozo')
->>>>>>> origin/master
+    archivo.write('Mi nombre es Alexander, mi compa se llama Ruben')
     archivo.close()
 grabartxt()
 
@@ -12,5 +8,5 @@ archivo=open('datos.txt','r')
 contenido=archivo.readline()
 b=contenido.replace(" ","")
 c=len(b)
-print ("Contenido del archivo:"+ contenido)
-print("Numero de letras : "+str(c))
+print ("Contenido del archivo: "+ contenido)
+print("Numero de letras: "+str(c))
