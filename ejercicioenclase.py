461ae23761bb1db02709e6a15d489cf3007551d7
Alexander Figueroa <alexdavid2405@hotmail.es>
2016-05-12 08:21:36 -0500

    caracteres
    contador de caracteres segun las frases ingresadas

--- ejercicioenclase.py
+++ ejercicioenclase.py
@@ -1,5 +1,9 @@
+z=0
 a=int(input("Cuantas frases desea ingresar: "))
 for i in range(1,a+1):
 	cadena=input("Ingrese frase "+str(i)+" :")
 	b=cadena.replace(" ","")
+	c=len(b)
+	z=z+c
 	print("Existen: "+str(len(b))+" letras.")
+print("Total de caracteres: "+str(z))
